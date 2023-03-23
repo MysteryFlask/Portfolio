f97e532d9bc821475640dafa9a480c396f4c7094
skyblocktoolsdev <76215428+skyblocktoolsdev@users.noreply.github.com>
2023-03-23 18:50:54 +0000

    Update AboutCard.js

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,7 +8,7 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            I am <span className="purple">MysteryFlask </span>
+            I am <span className="purple">MysteryFlask</span>
             , AKA Galaxy111, from <span className="purple"> London, England.</span>
             <br />I am a student currently pursuing a career in Programming, Software and App Development, and Data Science.
             <br />
