7c2cc0ad6ec9ea6697d606c477ce7dbf8807ad33
MysteryFlask <76215428+MysteryFlask@users.noreply.github.com>
2023-03-27 18:22:53 +0100

    Update linktree.js

--- src/components/LinkTree/linktree.js
+++ src/components/LinkTree/linktree.js
@@ -3,6 +3,7 @@ import { Container } from './styles';
 import Button from '../Button';
 import variables from '../../variables';
 import Header from '../Header';
+import Particle from "../Particle";
 
 const discordLogo = require('../../images/discord.svg');
 const githubLogo = require('../../images/github.svg');
@@ -13,6 +14,7 @@ const reactLogo = require('../../images/react.svg');
 export default function LinkTree() {
   return (
     <Container>
+      <Particle />
       <Header picture='https://scontent.fgvr2-1.fna.fbcdn.net/v/t31.0-8/p960x960/13710496_10209793534625506_2997078028056513758_o.jpg?_nc_cat=107&_nc_sid=7aed08&_nc_ohc=CT0MJiwW7cYAX-DuiSm&_nc_ht=scontent.fgvr2-1.fna&_nc_tp=6&oh=f10204fc396130e703c690b8be97f3c2&oe=5E94D2C2' title='Daniel Gomes Pereira' subtitle='A Brazilian Frontend Developer' />
       <Button link='https://www.youtube.com/@JMBGamesYT?view_as=subscriber' icon={youtubeLogo} name='Youtube' backgroundColor={variables.youtubeColor} />
       <Button link='https://github.com/mysteryflask' icon={githubLogo} name='Github' backgroundColor={variables.githubColor} />
