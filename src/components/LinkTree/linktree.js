83946d5377e75a5f5ac2714e0b0c450da6c6e4ff
MysteryFlask <76215428+MysteryFlask@users.noreply.github.com>
2023-03-29 13:36:37 +0100

    Update linktree.js

--- src/components/LinkTree/linktree.js
+++ src/components/LinkTree/linktree.js
@@ -9,12 +9,13 @@ import github from '../../images/github.svg'
 import youtube from '../../images/youtube.svg'
 import stack from '../../images/stack.svg'
 import react from '../../images/react.svg'
+import avatar from '../../Assets/avatar.png'
 
 export default function LinkTree() {
   return (
     <Container fluid className="project-section">
       <Particle />
-      <Header picture='https://scontent.fgvr2-1.fna.fbcdn.net/v/t31.0-8/p960x960/13710496_10209793534625506_2997078028056513758_o.jpg?_nc_cat=107&_nc_sid=7aed08&_nc_ohc=CT0MJiwW7cYAX-DuiSm&_nc_ht=scontent.fgvr2-1.fna&_nc_tp=6&oh=f10204fc396130e703c690b8be97f3c2&oe=5E94D2C2' title='Daniel Gomes Pereira' subtitle='A Brazilian Frontend Developer' />
+      <Header picture={avatar} title='MysteryFlask' subtitle='An Aspiring Developer' />
       <Button link='https://www.youtube.com/@JMBGamesYT?view_as=subscriber' icon={youtube} name='Youtube' backgroundColor={variables.youtubeColor} />
       <Button link='https://github.com/mysteryflask' icon={github} name='Github' backgroundColor={variables.githubColor} />
       <Button link='https://mysteryflask.vercel.app' icon={react} name='Website' backgroundColor={variables.opt2Color} />
