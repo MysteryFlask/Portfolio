eb611428475931870c0e9dad713c65a0673980e1
MysteryFlask <76215428+MysteryFlask@users.noreply.github.com>
2023-04-28 17:38:57 +0100

    Update linktree.js

--- src/components/LinkTree/linktree.js
+++ src/components/LinkTree/linktree.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container } from './styles';
+import { LinkContainer } from './styles';
 import Button from '../Button';
 import variables from '../../variables';
 import Header from '../Header';
@@ -10,17 +10,20 @@ import youtube from '../../images/youtube.svg'
 import stack from '../../images/stack.svg'
 import react from '../../images/react.svg'
 import avatar from '../../Assets/avatar.png'
+import { Container } from "react-bootstrap";
 
 export default function LinkTree() {
   return (
-    <Container fluid className="linktree-section">
+    <Container fluid className="linktree-bg">
       <Particle />
-      <Header picture={avatar} title='MysteryFlask' subtitle='An Aspiring Developer' style={{ color: 'white' }} />
-      <Button link='https://www.youtube.com/@JMBGamesYT?view_as=subscriber' icon={youtube} name='Youtube' backgroundColor={variables.youtubeColor} />
-      <Button link='https://github.com/mysteryflask' icon={github} name='Github' backgroundColor={variables.githubColor} />
-      <Button link='https://mysteryflask.vercel.app' icon={react} name='Website' backgroundColor={variables.opt2Color} />
-      <Button link='https://stackoverflow.com/users/14850720/mysteryflask' icon={stack} name='Stack Overflow' backgroundColor={variables.stackColor} />
-      <Button link='https://discord.com/channels/@me' icon={discord} name='Galaxy111#0001' backgroundColor={variables.discordColor} />
+      <LinkContainer fluid className="linktree-section">
+        <Header picture={avatar} title='MysteryFlask' subtitle='An Aspiring Developer' style={{ color: 'white' }} />
+        <Button link='https://www.youtube.com/@JMBGamesYT?view_as=subscriber' icon={youtube} name='Youtube' backgroundColor={variables.youtubeColor} />
+        <Button link='https://github.com/mysteryflask' icon={github} name='Github' backgroundColor={variables.githubColor} />
+        <Button link='https://mysteryflask.vercel.app' icon={react} name='Website' backgroundColor={variables.opt2Color} />
+        <Button link='https://stackoverflow.com/users/14850720/mysteryflask' icon={stack} name='Stack Overflow' backgroundColor={variables.stackColor} />
+        <Button link='https://discord.com/channels/@me' icon={discord} name='Galaxy111#0001' backgroundColor={variables.discordColor} />
+      </LinkContainer>
     </Container>
   )
 }
