2f14409a1d14153fab8da798052748ace4d782ca
MysteryFlask <76215428+MysteryFlask@users.noreply.github.com>
2023-05-02 12:12:15 +0100

    Update linktree.js

--- src/components/LinkTree/linktree.js
+++ src/components/LinkTree/linktree.js
@@ -13,14 +13,14 @@ import { Container } from './styles.js'
 
 export default function LinkTree() {
   return (
+    <Particle />
     <Container fluid className="linktree-bg">
-      <Particle />
-        <Header picture={avatar} title='MysteryFlask' subtitle='An Aspiring Developer' style={{ color: 'white' }} />
-        <Button link='https://www.youtube.com/@JMBGamesYT?view_as=subscriber' icon={youtube} name='Youtube' backgroundColor={variables.youtubeColor} />
-        <Button link='https://github.com/mysteryflask' icon={github} name='Github' backgroundColor={variables.githubColor} />
-        <Button link='https://mysteryflask.vercel.app' icon={react} name='Website' backgroundColor={variables.opt2Color} />
-        <Button link='https://stackoverflow.com/users/14850720/mysteryflask' icon={stack} name='Stack Overflow' backgroundColor={variables.stackColor} />
-        <Button link='https://discord.com/channels/@me' icon={discord} name='Galaxy111#0001' backgroundColor={variables.discordColor} />
+      <Header picture={avatar} title='MysteryFlask' subtitle='An Aspiring Developer' style={{ color: 'white' }} />
+      <Button link='https://www.youtube.com/@JMBGamesYT?view_as=subscriber' icon={youtube} name='Youtube' backgroundColor={variables.youtubeColor} />
+      <Button link='https://github.com/mysteryflask' icon={github} name='Github' backgroundColor={variables.githubColor} />
+      <Button link='https://mysteryflask.vercel.app' icon={react} name='Website' backgroundColor={variables.opt2Color} />
+      <Button link='https://stackoverflow.com/users/14850720/mysteryflask' icon={stack} name='Stack Overflow' backgroundColor={variables.stackColor} />
+      <Button link='https://discord.com/channels/@me' icon={discord} name='Galaxy111#0001' backgroundColor={variables.discordColor} />
     </Container>
   )
 }
