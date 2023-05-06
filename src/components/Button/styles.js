80f6dc4a6b36fc3ed64534003e267589e93f0786
MysteryFlask <76215428+MysteryFlask@users.noreply.github.com>
2023-05-06 14:05:08 +0100

    Update styles.js

--- src/components/Button/styles.js
+++ src/components/Button/styles.js
@@ -11,7 +11,7 @@ export const Container = styled.a`
   justify-content: center;
   margin: 10px auto;
   text-decoration: none;
-  width: 80px;
+  width: 200px;
   
   transition: background 0.4s ease-in-out;
 
