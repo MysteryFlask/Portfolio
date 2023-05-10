a880d4b5ae520e34387695adac2d8b2b7adecdb0
MysteryFlask <76215428+MysteryFlask@users.noreply.github.com>
2023-05-10 08:53:03 +0100

    Update blog.js

--- src/components/Blog/blog.js
+++ src/components/Blog/blog.js
@@ -1,13 +1,19 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Container } from "react-bootstrap";
 import Particle from "../Particle";
 
 const Blog = () => {
+  const [isScriptLoaded, setIsScriptLoaded] = useState(false);
+
   useEffect(() => {
     const script = document.createElement('script');
     script.src = 'https://substackapi.com/embeds/feed.js';
     script.async = true;
     document.body.appendChild(script);
+
+    script.onload = () => {
+      setIsScriptLoaded(true);
+    };
 
     return () => {
       document.body.removeChild(script);
@@ -27,20 +33,30 @@ const Blog = () => {
   }, []);
 
   return (
-    <Container fluid className="blog-main">
-      <Particle />
-      <Container>
-        <div class="substackcontent">
-          <h1 style={{ fontSize: "2.1em", paddingBottom: "20px" }}>
-              Who am I?
-          </h1>
-          <p style={{ marginBottom: "40px" }}>
-              This is my Substack blog written almost entirely by ChatGPT and AI. All I do is think of the topics...
-          </p>
-          <div id="substack-feed-embed"></div>
+    <>
+      {!isScriptLoaded && (
+        <div className="preloader">
+          {/* Add your preloader animation or image here */}
         </div>
-      </Container>
-    </Container>
+      )}
+
+      {isScriptLoaded && (
+        <Container fluid className="blog-main">
+          <Particle />
+          <Container>
+            <div className="substackcontent">
+              <h1 style={{ fontSize: "2.1em", paddingBottom: "20px" }}>
+                  Who am I?
+              </h1>
+              <p style={{ marginBottom: "40px" }}>
+                  This is my Substack blog written almost entirely by ChatGPT and AI. All I do is think of the topics...
+              </p>
+              <div id="substack-feed-embed"></div>
+            </div>
+          </Container>
+        </Container>
+      )}
+    </>
   );
 }
 
