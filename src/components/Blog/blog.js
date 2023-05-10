390da055b576fa6e704fc0f217dc870903da1dd6
MysteryFlask <76215428+MysteryFlask@users.noreply.github.com>
2023-05-10 08:55:42 +0100

    Update blog.js

--- src/components/Blog/blog.js
+++ src/components/Blog/blog.js
@@ -1,19 +1,13 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { Container } from "react-bootstrap";
 import Particle from "../Particle";
 
 const Blog = () => {
-  const [isScriptLoaded, setIsScriptLoaded] = useState(false);
-
   useEffect(() => {
     const script = document.createElement('script');
     script.src = 'https://substackapi.com/embeds/feed.js';
     script.async = true;
     document.body.appendChild(script);
-
-    script.onload = () => {
-      setIsScriptLoaded(true);
-    };
 
     return () => {
       document.body.removeChild(script);
@@ -33,30 +27,20 @@ const Blog = () => {
   }, []);
 
   return (
-    <>
-      {!isScriptLoaded && (
-        <div className="preloader">
-          {/* Add your preloader animation or image here */}
+    <Container fluid className="blog-main">
+      <Particle />
+      <Container>
+        <div class="substackcontent">
+          <h1 style={{ fontSize: "2.1em", paddingBottom: "20px" }}>
+              Who am I?
+          </h1>
+          <p style={{ marginBottom: "40px" }}>
+              This is my Substack blog written almost entirely by ChatGPT and AI. All I do is think of the topics...
+          </p>
+          <div id="substack-feed-embed"></div>
         </div>
-      )}
-
-      {isScriptLoaded && (
-        <Container fluid className="blog-main">
-          <Particle />
-          <Container>
-            <div className="substackcontent">
-              <h1 style={{ fontSize: "2.1em", paddingBottom: "20px" }}>
-                  Who am I?
-              </h1>
-              <p style={{ marginBottom: "40px" }}>
-                  This is my Substack blog written almost entirely by ChatGPT and AI. All I do is think of the topics...
-              </p>
-              <div id="substack-feed-embed"></div>
-            </div>
-          </Container>
-        </Container>
-      )}
-    </>
+      </Container>
+    </Container>
   );
 }
 
